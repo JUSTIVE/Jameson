9b750a51986da24cc374ee32ca255e9abc3f4fd7
Ben <ben399399@naver.com>
2021-08-18 22:40:58 +0900

    init on JsonParser

---
+++ Fragment/JsonParser.fs
@@ -0,0 +1,22 @@
+﻿module JsonParser
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+let joinKey (key:string) (parentPath:string):string = 
+    match parentPath with
+    | "" -> key
+    | _  -> $"{parentPath}.{key}"
+
+let rec keySet (jsonValue:JsonValue) (parentPath:string) (state:Set<string>):Set<string> = 
+    match jsonValue.Properties with
+    | [||] -> state
+    | propList ->
+        let applySetString ((key,value):(string*JsonValue)) = 
+            match parentPath with
+            | "" -> 
+                Set.add (joinKey key parentPath) state
+                |>keySet value key 
+        propList
+        |>Array.map applySetString 
+        |>Array.reduce(Seq.append)
+        |>Set.ofSeq
