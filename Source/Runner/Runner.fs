7ec7fc6c929438c7b5dc0678a82b0d735f7d7d12
Minsang Kim <ben399399@gmail.com>
2021-09-08 19:05:50 +0900

    wip on printer

--- Source/Runner/Runner.fs
+++ Source/Runner/Runner.fs
@@ -1,8 +1,10 @@
 ﻿module Runner
 open JamesonResult
 open JamesonOption
+open Printer
 
 let run (option:JamesonOption):JamesonResult =
+   printJamesonOption 0 option
    match option.runnerType with
    | TargetRunner(x:TargetRunner) ->
         TargetRunner.run x.sourcePath x.targetPath
